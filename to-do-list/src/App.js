6156f8929a5ed5f62bfe8c9655b8e5ec704f937d
devkonlak <kongarasu.l@gmail.com>
2024-03-22 18:46:49 +0530

    Contents shifted to app component and using props info send to Content component by using object destructuring props

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,14 +1,59 @@
-import  React from 'react';
-import Content from './Content';
-import Footer from './Footer'; 
-import Header from './Header';
-
+import React from "react";
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+import { useState } from "react";
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "Wake up by 5",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Practice Coding",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Sleep by 10",
+    },
+  ]);
+  {
+    /*defining default items inside useState as array of objects*/
+  }
+
+  const handleCheck = (id) => {
+    const listItem = items.map((task) =>
+      task.id === id ? { ...task, checked: !task.checked } : task
+    );
+    {
+      /* *Using the map function to iterate over the items array then checking if the id of the current task matches the provided id. If the id matches, creating a new task object with the checked property toggled to the opposite of its current value using !task.checked.If the id does not match, keeping the task object unchanged.*/
+    }
+    setItems(listItem);
+    localStorage.setItem("todo", JSON.stringify(listItem));
+  };
+
+  const handleDelete = (id) => {
+    const delTask = items.filter((task) => task.id !== id);
+    setItems(delTask);
+    {
+      /**Filtering the items array to exclude the task with the provided ID */
+    }
+    localStorage.setItem("todo", JSON.stringify(delTask));
+  };
   return (
-    <div className='App'>
-    <Header/>
-    <Content/>
-    <Footer/>
+    <div className="App">
+      <Header />
+      <Content 
+       items = {items}
+       handleCheck = {handleCheck}
+       handleDelete = {handleDelete}
+       />
+       {/** Sending the props to Content component */}
+      <Footer />
     </div>
   );
 }
--- to-do-list/src/Content.js
+++ to-do-list/src/Content.js
@@ -1,49 +1,12 @@
 import React from "react";
-import { useState } from "react";
 import { FaTrashArrowUp } from "react-icons/fa6";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "Wake up by 5",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Practice Coding",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Sleep by 10",
-    },
-  ]);
-  {
-    /*defining default items inside useState as array of objects*/
-  }
-
-  const handleCheck = (id) => {
-    const listItem = items.map((task) =>
-      task.id === id ? { ...task, checked: !task.checked } : task
-    );
-    {
-      /* *Using the map function to iterate over the items array then checking if the id of the current task matches the provided id. If the id matches, creating a new task object with the checked property toggled to the opposite of its current value using !task.checked.If the id does not match, keeping the task object unchanged.*/
-    }
-    setItems(listItem);
-    localStorage.setItem("todo",JSON.stringify(listItem))
-  };
-
-  const handleDelete = (id) => {
-      const delTask = items.filter( (task) => task.id !== id)
-      setItems(delTask)
-      {/**Filtering the items array to exclude the task with the provided ID */}
-      localStorage.setItem("todo",JSON.stringify(delTask))
-
-  };
+{/**{items,handleCheck,handleDelete} props are destructured from the props object in the function parameter list, allowing direct access to them within the component. */}
+const Content = ({items,handleCheck,handleDelete}) => {
+  
   return (
     <main>
+      {/**if the items.length has value display list or display "list is empty" */}
       {(items.length) ? (<ul>
       {/*using map to iterate the default items in unordered list */}
         {items.map((task) => (

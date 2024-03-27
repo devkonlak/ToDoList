7310225843f3cfab9acdae57e17cd3fa36d9da8e
devkonlak <kongarasu.l@gmail.com>
2024-03-27 11:48:53 +0530

    data folder is creatged and dumy data is stored. 3500 port war assigned to data and data is fetched inside app using useeffect

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,33 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
 import { useState } from "react";
 import Additem from "./Additem";
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todo')) || []);
-  {
-    /** localStorage.getItem('todo')Retrieves the value associated with the key 'todo' from local storage. 
-     * JSON.parse(...): Parses the retrieved value from local storage, converting it from a string to its JavaScript object representation.
-     * || [] If the previous step (JSON.parse(...)) fails (because localStorage.getItem('todo') returned null or undefined), the entire expression evaluates to an empty array []. This ensures that the code always has an array to work with, even if no to-do items were previously stored.
-    */
-  }
+  const API_URL = "http://localhost:3500/items"; // {/** */}
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("todo")) || []
+  );
+  //{
+  /** localStorage.getItem('todo')Retrieves the value associated with the key 'todo' from local storage.
+   * JSON.parse(...): Parses the retrieved value from local storage, converting it from a string to its JavaScript object representation.
+   * || [] If the previous step (JSON.parse(...)) fails (because localStorage.getItem('todo') returned null or undefined), the entire expression evaluates to an empty array []. This ensures that the code always has an array to work with, even if no to-do items were previously stored.
+   */
+  // }
+
+  useEffect(()=>{
+   const fetchItems = async()=>{
+    try{
+      const response = await fetch(API_URL);
+      const listTask = response.json();
+      setItems = listTask
+    }catch (err){
+
+    }
+   }
+  },[])
+
   const addTask = (item) => {
-    const id = items.length ? items[items.length -1].id+1 : 1; //Calculating the ID for a new item based on the length of the 'items' array.
-    const addNewTask = {id,checked:false,item} // creating addNewTask object
-    const newListItems = [...items, addNewTask] // addNewTask is added to the rest of the items 
+    const id = items.length ? items[items.length - 1].id + 1 : 1; //Calculating the ID for a new item based on the length of the 'items' array.
+    const addNewTask = { id, checked: false, item }; // creating addNewTask object
+    const newListItems = [...items, addNewTask]; // addNewTask is added to the rest of the items
     setItems(newListItems);
     localStorage.setItem("todo", JSON.stringify(newListItems));
-  }
-  
-  const [newTask,setNewTask] = useState(' ')
+  };
+
+  const [newTask, setNewTask] = useState(" ");
   const handleCheck = (id) => {
     const listItem = items.map((task) =>
       task.id === id ? { ...task, checked: !task.checked } : task
     );
-    {
-      /* *Using the map function to iterate over the items array then checking if the id of the current task matches the provided id. If the id matches, creating a new task object with the checked property toggled to the opposite of its current value using !task.checked.If the id does not match, keeping the task object unchanged.*/
-    }
+    //{
+    /* *Using the map function to iterate over the items array then checking if the id of the current task matches the provided id. If the id matches, creating a new task object with the checked property toggled to the opposite of its current value using !task.checked.If the id does not match, keeping the task object unchanged.*/
+    // }
     setItems(listItem);
     localStorage.setItem("todo", JSON.stringify(listItem));
   };
@@ -35,39 +51,36 @@ function App() {
   const handleDelete = (id) => {
     const delTask = items.filter((task) => task.id !== id);
     setItems(delTask);
-    {
-      /**Filtering the items array to exclude the task with the provided ID */
-    }
+    //{
+    /**Filtering the items array to exclude the task with the provided ID */
+    //}
     localStorage.setItem("todo", JSON.stringify(delTask));
   };
   const handleSubmit = (e) => {
-   e.preventDefault() // Prevent the default form submission behavior
-   if(!newTask) return;// checks if the newTask state variable is empty. If it's empty, the function returns early, indicating that no action should be taken.
-   console.log(newTask)
-   //addTask
-   addTask(newTask)
-   setNewTask('')
-   // Clearing the 'newTask' state variable by setting it to an empty string
-  }
+    e.preventDefault(); // Prevent the default form submission behavior
+    if (!newTask) return; // checks if the newTask state variable is empty. If it's empty, the function returns early, indicating that no action should be taken.
+    console.log(newTask);
+    //addTask
+    addTask(newTask);
+    setNewTask("");
+    // Clearing the 'newTask' state variable by setting it to an empty string
+  };
 
   return (
     <div className="App">
       <Header />
       <Additem
-      newTask = {newTask}
-      setNewTask = {setNewTask}
-      handleSubmit = {handleSubmit}
-
+        newTask={newTask}
+        setNewTask={setNewTask}
+        handleSubmit={handleSubmit}
       />
-      <Content 
-       items = {items}
-       handleCheck = {handleCheck}
-       handleDelete = {handleDelete}
-       />
-       {/** Sending the props to Content component */}
-      <Footer 
-      length = {items.length}
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
+      {/** Sending the props to Content component */}
+      <Footer length={items.length} />
     </div>
   );
 }

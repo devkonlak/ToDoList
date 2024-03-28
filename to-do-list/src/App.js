ee9f7320f20360a79e5e8bbf83b6072b24eaa0e7
devkonlak <kongarasu.l@gmail.com>
2024-03-28 06:57:04 +0530

    fetching  data from API

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,35 +6,36 @@ import { useState } from "react";
 import Additem from "./Additem";
 function App() {
   const API_URL = "http://localhost:3500/items"; // {/** */}
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("todo")) || []
-  );
+  const [items, setItems] = useState([]);
   //{
+  /* while receving the data from API we dont need to fetch the localStorage.
   /** localStorage.getItem('todo')Retrieves the value associated with the key 'todo' from local storage.
    * JSON.parse(...): Parses the retrieved value from local storage, converting it from a string to its JavaScript object representation.
    * || [] If the previous step (JSON.parse(...)) fails (because localStorage.getItem('todo') returned null or undefined), the entire expression evaluates to an empty array []. This ensures that the code always has an array to work with, even if no to-do items were previously stored.
    */
   // }
 
-  useEffect(()=>{
+  useEffect(() => {
     //An asynchronous function to fetch items from an API.
-   const fetchItems = async()=>{
-    try{
-      //// Sending a GET request to the API URL
-      const response = await fetch(API_URL);
-      const listTask = await response.json();
-      setItems(listTask)
-    }catch (err){
-      console.log (err.stack)
-    }
-   }
-   (async () =>await fetchItems())()
-  //  {/*
-  //  *(async () => await fetchItems()): This is an anonymous arrow function defined using the async function expression. Inside this function, fetchItems() is awaited, making sure that the asynchronous operation completes before proceeding.
-  //  * (): This function expression is immediately invoked because it's followed by (). This means the function is executed immediately after its definition.
-  //  */}
-  },[])
- 
+    const fetchItems = async () => {
+      try {
+        //// Sending a GET request to the API URL
+        const response = await fetch(API_URL);
+
+        const listTask = await response.json();
+
+        setItems(listTask);
+      } catch (err) {
+        console.log(err.stack);
+      }
+    };
+    (async () => await fetchItems())();
+    //  {/*
+    //  *(async () => await fetchItems()): This is an anonymous arrow function defined using the async function expression. Inside this function, fetchItems() is awaited, making sure that the asynchronous operation completes before proceeding.
+    //  * (): This function expression is immediately invoked because it's followed by (). This means the function is executed immediately after its definition.
+    //  */}
+  }, []);
+
   //{/** useEffect hook is being used to fetch items from an API when the component mounts, as indicated by the empty dependency array.*/}
 
   const addTask = (item) => {
@@ -42,7 +43,6 @@ function App() {
     const addNewTask = { id, checked: false, item }; // creating addNewTask object
     const newListItems = [...items, addNewTask]; // addNewTask is added to the rest of the items
     setItems(newListItems);
-    
   };
 
   const [newTask, setNewTask] = useState(" ");
@@ -54,7 +54,6 @@ function App() {
     /* *Using the map function to iterate over the items array then checking if the id of the current task matches the provided id. If the id matches, creating a new task object with the checked property toggled to the opposite of its current value using !task.checked.If the id does not match, keeping the task object unchanged.*/
     // }
     setItems(listItem);
-   
   };
 
   const handleDelete = (id) => {
@@ -63,12 +62,11 @@ function App() {
     //{
     /**Filtering the items array to exclude the task with the provided ID */
     //}
-    
   };
   const handleSubmit = (e) => {
     e.preventDefault(); // Prevent the default form submission behavior
     if (!newTask) return; // checks if the newTask state variable is empty. If it's empty, the function returns early, indicating that no action should be taken.
-   
+
     //addTask
     addTask(newTask);
     setNewTask("");
